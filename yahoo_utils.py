82114a4b0b9f4bb6763fc7952fa13c0bbc8550ff
elvicentina14 <elvicentina14@gmail.com>
2025-12-14 13:56:59 +0800

    Update yahoo_utils.py

--- yahoo_utils.py
+++ yahoo_utils.py
@@ -1,3 +1,5 @@
+# yahoo_utils.py
+
 def as_list(x):
     if x is None:
         return []
@@ -6,24 +8,50 @@ def as_list(x):
     return [x]
 
 
-def first_dict(items):
-    for x in as_list(items):
-        if isinstance(x, dict):
-            return x
-    return {}
+def first_dict(lst):
+    for item in as_list(lst):
+        if isinstance(item, dict):
+            return item
+    return None
 
 
-def find_all(items, key):
+def find_all(node, key):
     """
-    Recursively find ALL dicts that contain `key`
+    Recursively find ALL values for key inside Yahoo-style list/dict trees
     """
     found = []
-    for x in as_list(items):
-        if isinstance(x, dict):
-            if key in x:
-                found.extend(as_list(x[key]))
-            for v in x.values():
+
+    if isinstance(node, dict):
+        for k, v in node.items():
+            if k == key:
+                found.extend(as_list(v))
+            else:
                 found.extend(find_all(v, key))
-        elif isinstance(x, list):
-            found.extend(find_all(x, key))
+
+    elif isinstance(node, list):
+        for item in node:
+            found.extend(find_all(item, key))
+
     return found
+
+
+def extract_fragment(lst, key):
+    """
+    Scan list[dict] and return dict[key] if present
+    """
+    for frag in as_list(lst):
+        if isinstance(frag, dict) and key in frag:
+            return frag[key]
+    return None
+
+
+def extract_name(lst):
+    """
+    Extract name.full safely
+    """
+    for frag in as_list(lst):
+        if isinstance(frag, dict) and "name" in frag:
+            name_block = frag["name"]
+            if isinstance(name_block, list) and name_block:
+                return name_block[0].get("full")
+    return None
