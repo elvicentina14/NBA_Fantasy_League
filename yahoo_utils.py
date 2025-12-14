fb853cb784fba724cd20f2c03a9430d5c2d53091
elvicentina14 <elvicentina14@gmail.com>
2025-12-14 11:25:39 +0800

    Update yahoo_utils.py

--- yahoo_utils.py
+++ yahoo_utils.py
@@ -4,10 +4,6 @@ def as_list(x):
     return x if isinstance(x, list) else []
 
 def first_dict(x):
-    """
-    Return first dict inside list or dict itself.
-    Never throws.
-    """
     if isinstance(x, list):
         for i in x:
             if isinstance(i, dict):
