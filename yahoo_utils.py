a43d1464f3d7e1b3baf5149ad4ce65a79f05aa00
elvicentina14 <elvicentina14@gmail.com>
2025-12-14 09:40:55 +0800

    Update yahoo_utils.py

--- yahoo_utils.py
+++ yahoo_utils.py
@@ -1,3 +1,4 @@
+# yahoo_utils.py
 def ensure_list(x):
     if x is None:
         return []
@@ -6,13 +7,31 @@ def ensure_list(x):
     return [x]
 
 
-def unwrap(obj):
+def unwrap(node):
     """
-    Yahoo sometimes wraps objects as:
-    [int, {actual_data}]
+    Yahoo JSON returns:
+    - dict
+    - list
+    - int (!!!)
+    This normalizes everything to dict or list safely.
     """
-    if isinstance(obj, list):
-        if len(obj) == 2 and isinstance(obj[0], int):
-            return obj[1]
-        return obj[0]
-    return obj
+    if isinstance(node, dict):
+        return node
+    if isinstance(node, list):
+        return node
+    return {}
+
+
+def extract_players(container):
+    """
+    Handles:
+    players: { 0: {...}, 1: {...} }
+    players: [ {...}, {...} ]
+    """
+    out = []
+    if isinstance(container, dict):
+        for _, v in container.items():
+            out.append(v)
+    elif isinstance(container, list):
+        out.extend(container)
+    return out
